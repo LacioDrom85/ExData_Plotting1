ed9563ff8e2f1648a7b1e15e441cb4d69106e722
LacioDrom85 <francescoingino.85@gmail.com>
2015-11-07 20:29:08 +0100

    Plots and Codes

---
+++ Project1_Plot4.R
@@ -0,0 +1,67 @@
+########################
+#   COURSE PROJECT 1   #
+########################
+
+directory <- setwd("C:/Users/Francesco/OneDrive/Coursera/04 Exploratory Data Analysis/Quizs and Assignments")
+
+# Generate the folder "data"
+if(!file.exists("./data")){dir.create("./data")}
+
+# Extraxt files (in the "exdir") from the zip compression (from "zipfile").
+unzip(zipfile="Course_Project_1.zip",exdir="./data")
+
+# Upload of wd
+path_rf <- file.path(directory, "data")
+list.files(path_rf, recursive=TRUE)
+
+# Open Data
+EPC <- read.table(file.path(path_rf, "household_power_consumption.txt"),
+                  sep=";",
+                  header = FALSE,
+                  stringsAsFactors=FALSE,
+                  dec=".",
+                  skip=1)
+
+# Change names of variables
+names(EPC) <- c("Date", "Time", "GAP", "GRP", "Voltage", "GI", "SM1", "SM2", "SM3")
+
+# Changes Date and subsetting
+library(dplyr)
+EPC <- mutate(EPC, Date = as.Date(Date, "%d/%m/%Y"))
+Final_EPC <- filter(EPC, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+
+# Date and Time
+datetime <- paste(as.Date(Final_EPC$Date), Final_EPC$Time)
+Final_EPC$Datetime <- as.POSIXct(datetime)
+
+# Set variables as numeric
+Final_EPC$GAP <- as.numeric(Final_EPC$GAP, dec=".")
+Final_EPC$SM1 <- as.numeric(Final_EPC$SM1)
+Final_EPC$SM2 <- as.numeric(Final_EPC$SM2)
+Final_EPC$SM3 <- as.numeric(Final_EPC$SM3)
+Final_EPC$Voltage <- as.numeric(Final_EPC$Voltage)
+Final_EPC$GRP <- as.numeric(Final_EPC$GRP)
+
+#########
+# Plot 4:
+
+png("plot4.png", width=480, height=480, bg = "transparent")
+par(mfcol = c(2,2), mar = c(4,5,2,1))
+
+with(Final_EPC, {
+  # 1
+  plot(Final_EPC$GAP~Final_EPC$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  # 2
+  with(Final_EPC, plot(Datetime,SM1, main="", type="n", xlab = "", ylab = "Energy sub metering"))
+  with(Final_EPC, points(Datetime, SM1, col="black", type="l"))
+  with(Final_EPC, points(Datetime, SM2, col="red", type="l"))
+  with(Final_EPC, points(Datetime, SM3, col="blue", type="l"))
+  legend("topright", col = c("black", "red", "blue"),
+         lty=1, lwd=2, bty = "n",
+         legend = c("Sub Metering 1", "Sub Metering 2", "Sub Metering 3"))
+  # 3
+  plot(Final_EPC$Voltage~Final_EPC$Datetime, type="l", ylab="Voltage", xlab="Datetime")
+  # 4
+  plot(Final_EPC$GRP~Final_EPC$Datetime, type="l", ylab="Global Reactive Power", xlab="Datetime")
+})
+dev.off()
